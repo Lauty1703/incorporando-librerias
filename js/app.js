f07cc57ae9e2ae09264af533e79d0d2bb30ec28e
Lautaro fernandez <lautyfz1703@gmail.com>
2022-08-17 23:05:34 -0300

    incorporando libreria

--- js/app.js
+++ js/app.js
@@ -1,231 +1,189 @@
-/*
-Les dejo los links a cada librería, donde pueden encontrar los scripts para linkear desde cdn, documentación, etc.
-https://sweetalert2.github.io/
-https://apvarun.github.io/toastify-js/
-https://moment.github.io/luxon/#/
-https://nosir.github.io/cleave.js/
- */
+let carrito=[];
 
-const btnSwal = document.getElementById('botonSwal');
-const btnToast = document.getElementById('botonToast');
-let carrito = ['zanahorias', 'papas', 'pepinos', 'manzanas', 'naranjas', 'zucchini']
+let stockProductos=[{
+    id: 1,
+    peso:'1kg',
+    nombre: 'helado de crema de oreo ',
+    codigo: '0111202',
+    precio: 2500,
+    imagen: './img/oferta1.jpg',
+    descripcion_corta: 'helado de crema y oreo de 1 kilo',
+}, {
+    id: 2,
+    peso:'1kg',
+    nombre: ' cofler dulce de leche ',
+    codigo: '2100240',
+    precio: 2000,
+    imagen: './img/oferta2.jpg',
+    descripcion_corta: 'helado cofler de 1 kilo',
+},{
+    id: 3,
+    peso:'1kg',
+    nombre: 'helado de frutilla y crema',
+    codigo: '2195461',
+    precio: 1850,
+    imagen: './img/oferta3.jpg',
+    descripcion_corta: 'helado cofler de 1 kilo',
 
-btnSwal.onclick = () => {
-    /* Swal.fire(
-        {
-    title:'Bienvenida',
-    text:'Te damos la bienvenida a nuestra app',
-    icon:'success',
-    iconColor:'#81f40e',
-    confirmButtonText:'Gracias',
-    position:'top-center'
-    
-        }
-    ) */
+}
+,{
+    id: 4,
+    peso:'1kg',
+    nombre: 'helado de chocolate',
+    codigo: '2192034',
+    precio: 2000,
+    imagen: './img/oferta4.jpg',
+    descripcion_corta: 'helado coffee de 1 kilo',
+
+}
+,{
+    id: 5,
+    peso:'1kg',
+    nombre: 'galletas de chocolate',
+    codigo: '1694810',
+    precio: 1850,
+    imagen: './img/oferta5.jpg',
+    descripcion_corta: 'helado de galletas de chocolate 1 kilo',
+
+}
+,{
+    id: 6,
+    peso:'1kg',
+    nombre: 'helado de galleta de cholate',
+    codigo: '1964785',
+    precio: 1900,
+    imagen: './img/oferta6.jpg',
+    descripcion_corta: 'helado nestle de galleta de chocolate de 1 kilo',
+
+},
+{
+    id: 7,
+    peso:'1kg',
+    nombre: 'helado de galleta de cholate',
+    codigo: '1964785',
+    precio: 1900,
+    imagen: './img/oferta6.jpg',
+    descripcion_corta: 'helado nestle de galleta de chocolate de 1 kilo',
+
+},
+{
+    id: 8,
+    peso:'1kg',
+    nombre: 'helado de galleta de cholate',
+    codigo: '1964785',
+    precio: 1900,
+    imagen: './img/oferta1.jpg',
+
+}];
+const contenedorProductos=document.getElementById('contenedor-productos')
+const contenedorCarrito= document.getElementById('contadorCarrito');
+const botonVaciar=document.getElementById('vaciar-carrito')
+const contadorCarrito=document.getElementById('contadorCarrito')
+const precioTotal=document.getElementById('precioTotal')
 
 
-    /*  Swal.fire('Hola','Esto es un alert','info') */
 
-    Swal.fire({
+const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
+const botonAbrir = document.getElementById('boton-carrito')
+const botonCerrar = document.getElementById('carritoCerrar')
+const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
 
-        title: 'Eliminar producto',
-        text: '¿Está seguro de eliminar el producto?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Sí, seguro',
-        cancelButtonText: 'No, no quiero'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            //borrar();
-            Swal.fire('Borrado', 'El producto ha sido eliminado', 'success')
 
-        }
+
+
+
+botonVaciar.addEventListener('click',()=>{
+    carrito.length=0;
+    actualizarCarrito()
+})
+
+stockProductos.forEach((producto)=>{
+    const div=document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML=`
+    <img src=${producto.imagen} alt="">
+    <h3>${producto.nombre}</h3>
+    <p>peso: ${producto.peso}</p>
+    <p class="precioProducto">precio:$ ${producto.precio} </p>
+    <button id="agregar${producto.id}" class="boton-agregar">agregar<i class="fas fa-shopping-cart"></i></button>
+
+    `
+    contenedorProductos.appendChild(div)
+
+    const boton=document.getElementById(`agregar${producto.id}`)
+
+    boton.addEventListener('click',()=>{
+        agregarAlCarrito(producto.id)
+        Swal.fire(
+            {
+        title:'comprado',
+        text:'haz agregado un productoa tu carrito',
+        icon:'success',
+        iconColor:'#FA9191',
+        confirmButtonText:'gracias por su conpra',
+        position:'top-center'
+        
+            }
+        ) 
+        
     })
-}
-
-
-Toastify({
-    text: `Tenés ${carrito.length} productos en tu carrito`,
-    duration: 1500,
-    style: {
-        background: "radial-gradient(circle, rgba(235,0,255,1) 10%, rgba(86,9,121,1) 65%, rgba(68,0,50,1) 93%)",//Este gradiente lo hice con https://cssgradient.io/
-        color: 'white',
-        width: '20vw',
-        height: 80
-    },
-    offset: {
-        x: '50%',
-        y: 300,
-    },
-    onClick: () => {
-        Toastify({
-            text: 'Clickeaste un toast, yo te llevo a wikipedia',
-            duration: 1000,
-            position: 'left',
-            destination: 'https://wikipedia.org'
-        }).showToast();
-    }
-}).showToast()
-
-
-const DateTime = luxon.DateTime;
-
-const ahora = DateTime.now();
-console.log(ahora);
-
-
-const dt = DateTime.local(2022, 12, 8, 12, 15);
-console.log(dt);
-
-console.log(ahora.toString());
-console.log(ahora.toLocaleString());
-console.log(ahora.toLocaleString(DateTime.DATE_FULL));
-console.log(ahora.toLocaleString(DateTime.DATE_HUGE));
-console.log(ahora.toLocaleString(DateTime.TIME_SIMPLE));
-
-const parrafo = document.getElementById('horaActual');
-parrafo.innerText = ahora.toLocaleString(DateTime.TIME_SIMPLE);
-
-
-
-
-//Ejemplo integrador de librerías
-const btnCalcular = document.getElementById('calcular');
-
-window.onload = () => {
-    let dates = document.querySelectorAll('input[type="date"]');
-    let inicio = DateTime.now().toFormat('yyyy-MM-dd');
-    let fin = DateTime.now().plus({ months: 4 }).toFormat('yyyy-MM-dd');
-    dates.forEach(element => {
-        element.setAttribute("min", inicio);
-        element.setAttribute("max", fin);
-
-    });
-
-}
-
-function calcularEstadia(checkIn, checkOut) {
-    let total = checkOut.diff(checkIn);
-    return total.as('days');
-
-}
-
-function calcularPrecioTotal(estadia, precio) {
-    return estadia * precio;
-}
-btnCalcular.addEventListener('click', () => {
-    let checkIn = DateTime.fromISO(document.getElementById('checkIn').value);
-    let checkOut = DateTime.fromISO(document.getElementById('checkOut').value);
-    let estadia = calcularEstadia(checkIn, checkOut);
-    let precioTotal = calcularPrecioTotal(estadia, 2000);
-    Swal.fire({
-        title: 'Resultado',
-        text: `Tu estadía de ${estadia} días tiene un precio total de $${precioTotal}`,
-        icon: 'info',
-        iconColor: '#00ff00',
-        showCancelButton: true,
-        confirmButtonText: 'Confirmar',
-        cancelButtonText: 'Cancelar',
-        position: 'top-center',
-        backdrop: '#445566aa'
-    })
-
 })
 
 
-/* ---------------------------------- */
-//Código para tarjeta de crédito
-
-const selectAnio = document.querySelector('#anio'),
-    selectMes = document.querySelector('#mes'),
-    form = document.querySelector('#formulario'),
-    nombreTitular = document.querySelector('.inputNombre'),
-    numeroTarjeta = document.querySelector('.inputNumTarjeta'),
-    mesVencimiento = document.querySelector('.inputMes'),
-    anioVencimiento = document.querySelector('.inputAnio'),
-    codSeguridad = document.querySelector('.inputCodSeguridad'),
-    btnEnviar = document.querySelector('#btnEnviar'),
-    anios = [2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030],
-    meses = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-
-function cargarSelect(array, select) {
-    array.forEach(element => {
-        let option = `<option>${element}</option>`
-        select.innerHTML += option;
-    })
-}
-
-cargarSelect(anios, selectAnio);
-cargarSelect(meses, selectMes);
-
-function agregarContenido(ingreso, caja) {
-    ingreso.oninput = () => {
-        document.querySelector(caja).innerText = ingreso.value;
-    }
-}
-
-agregarContenido(numeroTarjeta, '.cajaNumTarjeta');
-agregarContenido(nombreTitular, '.cajaNombre');
-agregarContenido(mesVencimiento, '.mesVencimiento');
-agregarContenido(anioVencimiento, '.anioVencimiento');
-agregarContenido(codSeguridad, '.cajaCodSeguridad');
 
 
-codSeguridad.onmouseover = () => {
-    document.querySelector('.frente').style.transform = 'perspective(1000px) rotateY(-180deg)';
-    document.querySelector('.dorso').style.transform = 'perspective(1000px) rotateY(0deg)';
-}
-
-codSeguridad.onmouseout = () => {
-    document.querySelector('.frente').style.transform = 'perspective(1000px) rotateY(0deg)';
-    document.querySelector('.dorso').style.transform = 'perspective(1000px) rotateY(180deg)';
-}
 
 
-let cleave = new Cleave(numeroTarjeta, {
-    creditCard: true,
-    onCreditCardTypeChanged: (type) => {
-        const logos = document.querySelectorAll('.logoTarjeta');
-        logos.forEach(element => {
-            element.src = `./img/${type}.png`
-        })
-    }
+botonAbrir.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
+})
+botonCerrar.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
 })
 
-btnEnviar.addEventListener('click', (e) => {
-    e.preventDefault();
-
-
-    const data = {
-        nombre: nombreTitular.value,
-        numeroTarjeta: numeroTarjeta.value,
-        mes: mesVencimiento.value,
-        anio: anioVencimiento.value,
-        codSeguridad: codSeguridad.value
-    };
-
-
-    Swal.fire({
-        title: '¿Deseas guardar los datos de la tarjeta?',
-        showCancelButton: true,
-        position: 'top-center',
-        width: 400,
-        confirmButtonText: 'Guardar',
-
-    }).then((result) => {
-        formulario.reset();
-        if (result.isConfirmed) {
-            localStorage.setItem('data', JSON.stringify(data));
-            Swal.fire('Datos guardados', '', 'success');
-            setTimeout(() => {
-                location.reload();
-            }, 2000);
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-            Swal.fire('Los datos no se guardaron', 'Le tenes miedo al éxito', 'error');
-            setTimeout(() => {
-                location.reload();
-            }, 2000);
-        }
-    })
+contenedorModal.addEventListener('click', (event) =>{
+    contenedorModal.classList.toggle('modal-active')
 
 })
+modalCarrito.addEventListener('click', (event) => {
+    event.stopPropagation() 
+})
+
+
+
+
+const agregarAlCarrito=(prodId)=>{
+    const item=stockProductos.find((prod)=> prod.id===prodId)
+    carrito.push(item)
+    actualizarCarrito()
+    console.log(carrito)
+}
+
+
+const eliminarDelCarrito=(prodId)=>{
+    const item=carrito.find((prof)=> prod.id===prodId)
+
+    const indice=carrito.indexOf(item)
+    carrito.splice(indice,1)
+    actualizarCarrito()
+}
+
+
+const actualizarCarrito=()=>{
+
+    contenedorCarrito.innerHTML=""
+
+
+    carrito.forEach((prod)=>{
+        const div=document.createElement('div')
+        div.className=('productoEnCarrito')
+        div.innerHTML=`
+        <p>${prod.nombre}</p>
+        <p>precio: ${prod.precio}</p>
+        <p>cantidad: <spam id="cantidad"> ${prod.precio} </spam></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i>
+        `
+        contenedorCarrito.appendChild(div)
+    })
+    contadorCarrito.innerText=carrito.length
+}
